830956158c23ea5f22e377994a90e46c82254b52
unknown <brianlee.jpg@gmail.com>
2015-02-01 00:30:15 -0600

    added tweet retrieval

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 #INPUT = NUMBER OF TWEETS AND USER, OUTPUT = ARRAY OF INDIVIDUAL TWEET STRINGS
 #MAKE SURE TO OUTPUT AS TXT FILE
 
-
 import tweepy
 from tweepy.streaming import StreamListener
 from tweepy import Stream
+import json
 
 
 ckey = 'anBC9SHrBRMkah3vL0djtUzuv'
@@ -25,37 +25,58 @@ print('Name ' + user.name)
 
 class StdOutListener(StreamListener):
     ''' Handles data received from the stream. '''
- 
-    # def on_status(self, status):
-    #     # Prints the text of the tweet
-    #     print('Tweet text: ' + status.text)
- 
-    #     # There are many options in the status object,
-    #     # hashtags can be very easily accessed.
-    #     for hashtag in status.entries['hashtags']:
-    #         print(hashtag['text'])
- 
-    #     return true
- 
-    # def on_error(self, status_code):
-    #     print('Got an error with status code: ' + str(status_code))
-    #     return True # To continue listening
- 
-    # def on_timeout(self):
-    #     print('Timeout...')
-    #     return True # To continue listening
+
+    def __init__(self, max_results): 
+        super(StdOutListener, self).__init__()
+        self.text_output = open('tweet_text', 'w')
+        self.date_output = open('tweet_dates', 'w')
+
+        self.count = 0
+        self.max_count = max_results
+
+        if max_results:
+            self.max_count = max_results
+        else: 
+            self.max_count = float("inf")
 
     def on_data(self, data):
-    	print data
-    	return True
+
+        ## if we have not hit the limit, write all applicable data
+        if self.count < self.max_count: 
+
+            jd = json.loads(data)
+            if self.count < self.max_count:
+
+                if jd.has_key('text'):
+                    self.text_output.write(jd['text'].encode('utf-8'))
+                if jd.has_key('lang'):
+                    self.text_output.write(jd['lang'])
+                if jd.has_key('created_at'):
+                    self.date_output.write(jd['created_at'].encode('utf-8'))
+
+                if jd.has_key('text') or jd.has_key('lang'):
+                    self.text_output.write("\n")
+                    self.date_output.write("\n")
+
+            ## increment
+            self.count += 1
+            return True
+        else:
+            self.text_output.close()
+            self.date_output.close()
+            print 'tweets = '+str(self.count)
+            return False
+
+
+    # 	return True
 
     def on_error(self, status):
     	print status
 
 
 def extractPosts(keyword):
-	twitterStream = Stream(auth, StdOutListener())
-	twitterStream.filter(track=[keyword])
+    twitterStream = Stream(auth, StdOutListener(3))
+    twitterStream.filter(track=[keyword]) 
 
 	#need to extract only posts and output to .txt file
  
